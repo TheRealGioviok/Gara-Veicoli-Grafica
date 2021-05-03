5d6aebc23282c2868b3cc174278081e61dc21e00
BuildTools <unconfigured@null.spigotmc.org>
2021-05-03 19:29:25 +0200

    .

--- gara.h
+++ gara.h
@@ -7,6 +7,7 @@
 namespace gv {
 	class Gara {
 		double len;
+
 		Array<Veicolo> arr;
 
 		void setLen(double len) {
@@ -18,6 +19,25 @@ namespace gv {
 			return len;
 		}
 
+		Veicolo operator[](int index) {
+			if (index<0) throw invalid_argument("Invalid index argument in gv::Gara[] : " + std::string(index));
+			if (index >= arr.size()) throw invalid_argument("Invalid index argument in gv::Gara[] (list index out of range) : " + std::string(index));
+			return arr[index];
+		}
+
+		void sort() { //Just a simple bubble sort, nothing too difficult
+			bool sorted;
+			int opt = 0; //Optimization lel
+			do {
+				sorted = true;
+				for (int i = 0; i < arr.size()-1-(opt++); i++) {
+					if (arr[i] > arr[i + 1]) {
+						sorted = false;
+						arr.swap(i, i + 1);
+					}
+				}
+			} while (!sorted);
+		}
 	};
 }
 
